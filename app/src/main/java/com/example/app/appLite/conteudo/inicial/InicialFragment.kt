884e10f3d1b5af347778da6fc24472fee593dbf8
ritadecassiads <ritadamascenosantos3@gmail.com>
2023-10-19 09:38:58 -0300

    Alteração no Inicial

--- app/src/main/java/com/example/app/appLite/conteudo/inicial/InicialFragment.kt
+++ app/src/main/java/com/example/app/appLite/conteudo/inicial/InicialFragment.kt
@@ -7,12 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.app.R
+import com.example.app.databinding.FragmentInicialBinding
 
 class InicialFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_inicial, container, false)
+        val binding = FragmentInicialBinding.inflate(layoutInflater)
+
+        return binding.root
     }
 }
