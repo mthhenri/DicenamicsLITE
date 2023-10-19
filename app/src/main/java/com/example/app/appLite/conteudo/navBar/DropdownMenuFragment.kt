7c541a2070ec19a8012f59b8046e4b213b527eac
blublui <blabla>
2023-10-19 09:45:21 -0300

    criação do nav bar menu

---
+++ app/src/main/java/com/example/app/appLite/conteudo/navBar/DropdownMenuFragment.kt
@@ -0,0 +1,20 @@
+package com.example.app.appLite.conteudo.navBar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.app.databinding.FragmentDropdownMenuBinding
+
+class DropdownMenuFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentDropdownMenuBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+}
