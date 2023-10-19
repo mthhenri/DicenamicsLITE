c556e52ef5210499992c127a9ac60d13e6a76594
aaaaa <aaaaa@aaaa.br>
2023-10-19 09:33:53 -0300

    Criação de InicialFragment

---
+++ app/src/main/java/com/example/app/appLite/conteudo/inicial/InicialFragment.kt
@@ -0,0 +1,18 @@
+package com.example.app.appLite.conteudo.inicial
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.app.R
+
+class InicialFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_inicial, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/app/appLite/conteudo/inicial/InicialViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.app.appLite.conteudo.inicial
+
+import androidx.lifecycle.ViewModel
+
+class InicialViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
