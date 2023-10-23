571e8122bc3f10f08e8399e8e95e8afff4ea687d
mthhenri <mathsilvaesilva@gmail.com>
2023-10-23 12:37:47 -0300

    Criação do Banco e Repositorys

---
+++ app/src/main/java/com/example/app/data/database/DatabaseSQLite.kt
@@ -0,0 +1,6 @@
+package com.example.app.data.database
+
+import androidx.room.Database
+
+abstract class DatabaseSQLite {
+}
---
+++ app/src/main/java/com/example/app/data/repository/DadoRepository.kt
@@ -0,0 +1,4 @@
+package com.example.app.data.repository
+
+class DadoRepository {
+}
