e7cf185ff0937d2ba3a99f41ef52232fd4db7eb8
RafasPorto <rafaelkporto@gmail.com>
2023-10-25 14:56:22 -0300

    tentativa de fazer a lista de pastas

---
+++ app/src/main/java/com/example/app/appLite/ui/pastas/PastasAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.app.appLite.ui.pastas
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.app.data.models.Pasta
+
+class PastasAdapter (
+        private var pastas : List<Pasta>,
+        val viewModel: PastasViewModel
+    ) :
+    RecyclerView.Adapter<PastasAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PastasAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PastasAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
